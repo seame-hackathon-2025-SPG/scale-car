2133cfe0b24e4e13df1a8ea6143e29d1ef6d251b
YeonWooKimm <kyw10987@gmail.com>
2025-07-14 02:31:37 +0900

    fix:오류수정

--- src/integration/setup.py
+++ src/integration/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'integration'
 
@@ -10,11 +12,13 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # launch 파일 포함
+        (os.path.join('share', package_name, 'launch'), glob('integration/launch/*.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='kyw10987',
-    maintainer_email='kyw10987@gmail.com',
+    maintainer='spg1234',
+    maintainer_email='spg1234@todo.todo',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
