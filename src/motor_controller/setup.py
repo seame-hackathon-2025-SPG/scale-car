6bfefeec7783d0e95bfea60c94a040d94a45386e
YeonWooKimm <kyw10987@gmail.com>
2025-07-13 19:54:55 +0900

    feature: 모터 컨트롤러 노드작성

---
+++ src/motor_controller/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'motor_controller'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='kyw10987',
+    maintainer_email='kyw10987@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'motor_controller_pub_node = motor_controller.pub_node.publisher:main',
+            'motor_controller_sub_node = motor_controller.sub_node.subscriber:main'
+        ],
+    },
+)
