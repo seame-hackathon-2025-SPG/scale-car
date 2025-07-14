005047954567f8ada95987fd3f30e5cfc8c60641
KimSeongHyeon <nicesh0571@gmail.com>
2025-07-15 02:50:05 +0900

    change lane_detection_subscriber.py

--- src/camera_node/subscriber/lane_detection_subscriber.py
+++ src/camera_node/subscriber/lane_detection_subscriber.py
@@ -28,27 +28,28 @@ class LaneDetectionNode(Node):
         height, width, _ = frame.shape
 
         # 1️⃣ ROI: 아래쪽 절반
-        roi = frame[height//2:, :]
+        roi = frame[int(height * 2/3):, :]
 
         # 2️⃣ grayscale → blur → Canny edge
         gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
         blur = cv2.GaussianBlur(gray, (5, 5), 0)
-        edges = cv2.Canny(blur, 50, 150)
+        edges = cv2.Canny(blur, 45, 120)
 
         # 3️⃣ HoughLinesP
-        lines = cv2.HoughLinesP(edges, 1, np.pi/180, 50, minLineLength=50, maxLineGap=50)
+        lines = cv2.HoughLinesP(edges, 1, np.pi/180, 30, minLineLength=80, maxLineGap=100)
         left_slopes, right_slopes, left_x, right_x = [], [], [], []
 
         if lines is not None:
             for line in lines:
                 x1, y1, x2, y2 = line[0]
                 slope = (y2 - y1) / (x2 - x1 + 1e-6)  # prevent division by zero
+                self.get_logger().info(f'line: {line[0]}, slope: {slope:.2f}')
                 if abs(slope) < 0.5:  # 수평선 무시
                     continue
-                if slope > 0 and x1 < width / 2 and x2 < width / 2:
+                if x1 < width / 2 and x2 < width / 2:
                     left_slopes.append(slope)
                     left_x.extend([x1, x2])
-                elif slope < 0 and x1 > width / 2 and x2 > width / 2:
+                elif x1 > width / 2 and x2 > width / 2:
                     right_slopes.append(slope)
                     right_x.extend([x1, x2])
 
@@ -69,6 +70,30 @@ class LaneDetectionNode(Node):
         self.publisher_.publish(msg)
 
         # (선택) 시각화
+        debug = roi.copy()
+        for line in lines:
+            x1, y1, x2, y2 = line[0]
+            slope = (y2 - y1) / (x2 - x1 + 1e-6)
+
+            # 수평 (빨간색), 수직 (파란색)만 그리기
+            if abs(slope) < 0.5:
+                continue
+            elif abs(slope) >= 0.5:
+                
+                if x1 < width / 2 and x2 < width / 2:
+                    color = (255, 0, 0)  # 파란색
+                    cv2.line(debug, (x1, y1), (x2, y2), color, 2)
+                    left_slopes.append(slope)
+                    left_x.extend([x1, x2])
+                elif x1 > width / 2 and x2 > width / 2:
+                    color = (0, 0, 255)  # 파란색
+                    cv2.line(debug, (x1, y1), (x2, y2), color, 2)
+                    right_slopes.append(slope)
+                    right_x.extend([x1, x2])
+            else:
+                continue  # 초록색 대신 아예 무시
+            
+        cv2.imshow('Hough', debug)
         cv2.imshow('Lane Detection', edges)
         cv2.waitKey(1)
 
